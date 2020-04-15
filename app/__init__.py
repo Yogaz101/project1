4a246326c5c0dcf81c6513356661fd34bf72c4e1
Yogaz101 <43712799+Yogaz101@users.noreply.github.com>
2020-04-15 15:40:41 -0500

    second

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ Uploads = "./app/static/profilepics"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234@localhost/lab5"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://gztpvcswgnpipg:63c75dbb9f460474fbbe9c471a36bfbf6c71187b120a202258f68beb7e9bf4b3@ec2-35-174-88-65.compute-1.amazonaws.com:5432/d9e3ql54457peh"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['Uploads'] = Uploads
 
